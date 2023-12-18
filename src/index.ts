a2aa028552d8374dbefe773c81035b3b91e6056d
Aralhi <xiaoping0501@gmail.com>
2023-12-19 00:16:56 +0800

    feat: add custom css

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as Chart } from './components/Chart';
 export { default as Custom } from './components/Custom';
 export { default as Reference } from './components/Reference';
 export { default as Summary } from './components/Summary';
+import './index.css';
